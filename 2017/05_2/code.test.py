a7bb7679fafefc400f73bd677861d21a7561fc0f
Mátyás Budavári <maty190@gmail.com>
2017-12-05 09:59:44 +0100

    2017 Day 5 / 2

---
+++ 2017/05_2/code.test.py
@@ -0,0 +1,14 @@
+""" Advent of code 2017	day 5/2	"""
+
+import unittest
+from code import solution
+
+class MyTest(unittest.TestCase):
+    """Unit tests for actual day"""
+
+    def test_match(self):
+        """ The basic test cases """
+        self.assertEqual(solution("0\n3\n0\n1\n-3"), 10)
+
+if __name__ == '__main__':
+    unittest.main()
