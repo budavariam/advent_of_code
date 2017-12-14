a8231c8cb744cb74cd17c7623c1fee435da0a5b4
Mátyás Budavári <maty190@gmail.com>
2017-12-14 12:43:42 +0100

    2017 day 14 / 1

---
+++ 2017/14_1/code.py
@@ -0,0 +1,31 @@
+""" Advent of code 2017 day 14/1 """
+from argparse import ArgumentParser
+from functools import reduce
+from knot import Knot
+
+def convert(line):
+    """ Convert a line to binary representation """
+    return ''.join(bin(int(line, 16))[2:].zfill(128))
+
+def count_used(matrix):
+    """ Count the 1-s in the lines """
+    return sum([line.count('1') for line in matrix])
+
+def solution(input_data):
+    """ Solution to the problem """
+    matrix = [convert(Knot(256, '{}-{}'.format(input_data, index)).knot_hash()) for index in range(128)]
+    return count_used(matrix)
+
+if __name__ == "__main__":
+    PARSER = ArgumentParser()
+    PARSER.add_argument("--input", dest='input', action='store_true')
+    PARSER.add_argument("--test")
+    ARGS = PARSER.parse_args()
+    if ARGS.input:
+        with(open('input.txt', 'r')) as input_file:
+            print(solution(input_file.read()))
+    elif ARGS.test:
+        print(solution(str(ARGS.test)))
+    else:
+        DEBUG = """flqrgnkx"""
+        print(solution(DEBUG))
