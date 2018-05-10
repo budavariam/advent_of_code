0b0ad45e348a519418afe88fb90f2bd8d829f26d
Mátyás Budavári <budavariam@gmail.com>
2018-05-11 00:10:53 +0200

    2016 day 01 / 2

---
+++ 2016/01_2/solution.ts
@@ -0,0 +1,100 @@
+export function solution(input: string[]) {
+    const parsed = parseInput(input[0]);
+    const coords = processData(parsed);
+    const distance = calcDistance(coords);
+    return "" + Math.abs(distance);
+}
+
+function parseInput(input: string): Direction[] {
+    return input
+        .split(", ")
+        .map((elem: string) => new Direction(elem.charAt(0) === "R" ? "R" : "L", parseInt(elem.slice(1), 10)));
+}
+
+function processData(parsed: Direction[]): CoordNumbers {
+    const position = new Position();
+    for (const element of parsed) {
+        const notFoundIntersection = position.move(element);
+        if (!notFoundIntersection) {
+            break;
+        }
+    }
+    // console.log(position.currentPosition());
+    return position.currentPosition();
+}
+
+function calcDistance(coords: CoordNumbers): number {
+    return Math.abs(coords[0]) + Math.abs(coords[1]);
+}
+
+type Quarter = "N" | "E" | "W" | "S";
+type Dirs = "R" | "L";
+type Coords = "x" | "y";
+type CoordNumbers = [number, number];
+type DirectionMap = { [k in Quarter]: { [l in Dirs]: Quarter } };
+type StepMap = { [k in Quarter]: [Coords, number] };
+
+class Direction {
+    constructor(
+        public direction: Dirs,
+        public count: number) {}
+}
+
+class Position {
+    public visited: Set<string> = new Set();
+    private x = 0;
+    private y = 0;
+    private direction: Quarter = "N";
+    private directions: DirectionMap = {
+        E: {R: "S", L: "N"},
+        N: {R: "E", L: "W"},
+        S: {R: "W", L: "E"},
+        W: {R: "N", L: "S"},
+    };
+    private step: StepMap = {
+        E: ["x",  1],
+        N: ["y",  1],
+        S: ["y", -1],
+        W: ["x", -1],
+    };
+    constructor() {}
+
+    public currentPosition(): CoordNumbers {
+        return [this.x, this.y];
+    }
+
+    public move(newDir: Direction) {
+        this.turn(newDir.direction);
+        return this.adjustPosition(newDir.count);
+    }
+
+    private turn(direction: Dirs) {
+        this.direction = this.directions[this.direction][direction];
+    }
+
+    private adjustPosition(count: number) {
+        const moveBy = this.step[this.direction];
+        if (moveBy[0] === "x") {
+            for (let i = 0; i < count; i++) {
+                const currentPosition = `${this.x}_${this.y}`;
+                if (!this.visited.has(currentPosition)) {
+                    this.x += moveBy[1];
+                    this.visited.add(currentPosition);
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            for (let i = 0; i < count; i++) {
+                const currentPosition = `${this.x}_${this.y}`;
+                if (!this.visited.has(currentPosition)) {
+                    this.y += moveBy[1];
+                    this.visited.add(currentPosition);
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ 2016/01_2/test.ts
@@ -0,0 +1,8 @@
+import { checkResult } from "../utils/checkResult";
+import { solution } from "./solution";
+
+const data = [
+    {input: ["R8, R4, R4, R8"], expected: "4"},
+];
+
+checkResult(solution, data);
