ece6bfc239efccbae9973da136547d596312eb96
Mátyás Budavári <maty190@gmail.com>
2018-05-19 15:07:52 +0200

    2016 day 07 / 1 tslint warning eliminate

--- 2016/07_1/solution.ts
+++ 2016/07_1/solution.ts
@@ -11,27 +11,27 @@ const isEven = (_: any, index: number) => index % 2 === 1;
 const isOdd =  (_: any, index: number) => index % 2 === 0;
 
 class IPv7 {
-    private checkABBA(input: string) {
-        for (let i = 3; i < input.length; i++) {
-            if (input[i] === input[i-3] && input[i] !== input[i-2] && input[i-2] === input[i-1]) {
-                return true;
-            }
-        }
-        return false;
-    }
+    public supportsTLS = false;
 
     constructor(private line: string) {
         const parts = line.split(/\[|\]/); // odd index: bracketed; even: outside
-        
+
         const inBrackets = parts
             .filter(isEven)
             .some(this.checkABBA);
         const outsideBrackets = parts
             .filter(isOdd)
             .some(this.checkABBA);
-        
+
         this.supportsTLS = !inBrackets && outsideBrackets;
     }
 
-    public supportsTLS = false;
+    private checkABBA(input: string) {
+        for (let i = 3; i < input.length; i++) {
+            if (input[i] === input[i - 3] && input[i] !== input[i - 2] && input[i - 2] === input[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
