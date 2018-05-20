02e209681cf572c38de89fae978f3213dae37524
Mátyás Budavári <maty190@gmail.com>
2018-05-20 14:31:19 +0200

    2016 day 10 / 2

---
+++ 2016/10_2/index.ts
@@ -0,0 +1,8 @@
+import { evaluateInput } from "../utils/evaluateInput";
+import { solution } from "./solution";
+
+const options = {
+    outputIDs: [0, 1, 2],
+};
+
+evaluateInput(solution, __dirname, options);
---
+++ 2016/10_2/test.ts
@@ -0,0 +1,15 @@
+import { checkResult } from "../utils/checkResult";
+import { solution } from "./solution";
+
+const data = [
+    {input: [
+        "value 5 goes to bot 2",
+        "bot 2 gives low to bot 1 and high to bot 0",
+        "value 3 goes to bot 1",
+        "bot 1 gives low to output 1 and high to bot 0",
+        "bot 0 gives low to output 2 and high to output 0",
+        "value 2 goes to bot 2",
+    ], expected: "6"},
+];
+
+checkResult(solution, data, {outputIDs: [1, 2]});
