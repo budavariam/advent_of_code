cd4ceaba88e95d601b86b8b5826d441794610f12
Mátyás Budavári <budavariam@gmail.com>
2018-05-30 01:12:54 +0200

    2016 day 14 / 2

---
+++ 2016/14_2/test.ts
@@ -0,0 +1,11 @@
+import { checkResult } from "../utils/checkResult";
+import { solution } from "./solution";
+
+const data = [
+    {
+        input: ["abc"],
+        expected: "22551",
+    },
+];
+
+checkResult(solution, data);
