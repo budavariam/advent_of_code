1c6f32d7764200968826ef11f52e81d02e7b9420
Mátyás Budavári <maty190@gmail.com>
2018-12-03 09:50:50 +0100

    2018 day 03 / 2

---
+++ 2018/03_2/solution.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"advent_of_code/2018/utils"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+func main() {
+	data := utils.LoadInput("03_2/input.txt")
+	result := CalculateNonOverlappingFabric(data, 1000, 1000)
+	fmt.Println(result)
+}
+
+// CalculateNonOverlappingFabric calculates which fabric does not overlap
+func CalculateNonOverlappingFabric(data []string, width int, height int) int {
+	fabricMatrix := InitMatrix(width, height)
+	overlaps := map[int]bool{}
+	for _, line := range data {
+		regex := *regexp.MustCompile(`#(\d+) @ (\d+),(\d+): (\d+)x(\d+)`)
+		parsedData := regex.FindAllStringSubmatch(line, -1)
+		FillMatrix(fabricMatrix, parsedData, overlaps)
+	}
+	nonoverlapping := FindNonOverlapping(overlaps)
+	return nonoverlapping
+}
+
+// InitMatrix initializes a 2d slice with false values.
+func InitMatrix(width int, height int) [][]int {
+	matrix := make([][]int, height)
+	for y := 0; y < height; y++ {
+		matrix[y] = make([]int, width)
+		for x := 0; x < width; x++ {
+			matrix[y][x] = 0
+		}
+	}
+	return matrix
+}
+
+// FillMatrix marks the fabric of what to cut, and sets the overlaps
+func FillMatrix(matrix [][]int, res [][]string, overlaps map[int]bool) {
+	index, _ := strconv.Atoi(res[0][1])
+	leftm, _ := strconv.Atoi(res[0][2])
+	topm, _ := strconv.Atoi(res[0][3])
+	width, _ := strconv.Atoi(res[0][4])
+	height, _ := strconv.Atoi(res[0][5])
+	if !overlaps[index] {
+		// Make sure that the current index is in the 'set'.
+		overlaps[index] = false
+	}
+	for y := topm; y < topm+height; y++ {
+		for x := leftm; x < leftm+width; x++ {
+			if matrix[y][x] != 0 {
+				// Mark the two overlapping fabrics in the overlaps set
+				overlaps[matrix[y][x]] = true
+				overlaps[index] = true
+			}
+			// Set which fabric lays in that place
+			matrix[y][x] = index
+		}
+	}
+}
+
+// FindNonOverlapping returns the only one false value from the overlaps set
+func FindNonOverlapping(overlaps map[int]bool) int {
+	for index, isOverlapping := range overlaps {
+		if !isOverlapping {
+			return index
+		}
+	}
+	return -1
+}
---
+++ 2018/03_2/solution_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"advent_of_code/2018/utils"
+	"testing"
+)
+
+func TestProvided(t *testing.T) {
+	data := []utils.TestcaseNum{
+		{Inp: []string{"#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"}, Exp: 3},
+	}
+
+	for index, testcase := range data {
+		result := CalculateNonOverlappingFabric(testcase.Inp, 11, 11)
+		if result != testcase.Exp {
+			t.Errorf("%dth test was incorrect, got: %d, expected: %d.", index, result, testcase.Exp)
+		}
+	}
+}
