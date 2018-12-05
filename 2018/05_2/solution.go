54f6a43a63a73027fa842e582857554aa9ae585f
Mátyás Budavári <maty190@gmail.com>
2018-12-05 15:43:28 +0100

    2018 day 05 / 2

---
+++ 2018/05_2/solution.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"advent_of_code/2018/utils"
+	"fmt"
+)
+
+func main() {
+	data := utils.LoadInput("05_1/input.txt")
+	result := MinimalPolimer(data[0])
+	fmt.Println(result)
+}
+
+// MinimalPolimer will return the least possible polimer by this elimination rule by eliminating one type of polimer completely
+func MinimalPolimer(data string) int {
+	minimalLength := len(data)
+	for _, char := range "abcdefghijklmnopqrstuvwxyz" {
+		current := LengthAfterReactions(data, char)
+		if current < minimalLength {
+			minimalLength = current
+		} else if current == 0 {
+			return 0
+		}
+	}
+	return minimalLength
+
+}
+
+// LengthAfterReactions plays the reactions in the polimer and returns its final length
+func LengthAfterReactions(polymer string, ignore rune) int {
+	var stack []rune
+	n := 0
+	for _, char := range polymer {
+		if char == ignore || char == swapPolarity(ignore) {
+			continue
+		} else if n > 0 && stack[n-1] == swapPolarity(char) {
+			/* if the current char is in the stack with a different polarity, pop the last char from stack,
+			by not adding it to the stack, it eliminates the previous and the current as well. */
+			stack = stack[:n-1]
+			n--
+		} else {
+			stack = append(stack, char)
+			n++
+		}
+	}
+	return n
+}
+
+func swapPolarity(char rune) rune {
+	if char <= 'Z' && char >= 'A' {
+		return char - 'A' + 'a'
+	}
+	return char - 'a' + 'A'
+}
