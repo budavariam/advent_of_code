8c80acdae6790f080541448be2934c4a2bd0b08a
Mátyás Budavári <maty190@gmail.com>
2018-12-11 01:09:26 +0100

    2018 day 04 / 1

---
+++ 2018/04_1/solution_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"advent_of_code/2018/utils"
+	"testing"
+)
+
+func TestProvided(t *testing.T) {
+	data := []utils.TestcaseNum{
+		{Inp: []string{
+			"[1518-11-01 00:00] Guard #10 begins shift",
+			"[1518-11-01 00:05] falls asleep",
+			"[1518-11-01 00:25] wakes up",
+			"[1518-11-01 00:30] falls asleep",
+			"[1518-11-01 00:55] wakes up",
+			"[1518-11-01 23:58] Guard #99 begins shift",
+			"[1518-11-02 00:40] falls asleep",
+			"[1518-11-02 00:50] wakes up",
+			"[1518-11-03 00:05] Guard #10 begins shift",
+			"[1518-11-03 00:24] falls asleep",
+			"[1518-11-03 00:29] wakes up",
+			"[1518-11-04 00:02] Guard #99 begins shift",
+			"[1518-11-04 00:36] falls asleep",
+			"[1518-11-04 00:46] wakes up",
+			"[1518-11-05 00:03] Guard #99 begins shift",
+			"[1518-11-05 00:45] falls asleep",
+			"[1518-11-05 00:55] wakes up"}, Exp: 240},
+	}
+
+	for index, testcase := range data {
+		result := ChooseGuardAndMinute(testcase.Inp)
+		if result != testcase.Exp {
+			t.Errorf("%dth test was incorrect, got: %d, expected: %d.", index, result, testcase.Exp)
+		}
+	}
+}
