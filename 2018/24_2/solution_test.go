658952436b20e5ff505af075a0cc21a4787540b4
Mátyás Budavári <maty190@gmail.com>
2019-09-10 22:06:25 +0200

    2018 day 24 / 2

---
+++ 2018/24_2/solution_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/budavariam/advent_of_code/2018/utils"
+)
+
+func TestProvided(t *testing.T) {
+	data := []utils.TestcaseNum{
+		{
+			Inp: []string{
+				"Immune System:",
+				"17 units each with 5390 hit points (weak to radiation, bludgeoning) with an attack that does 4507 fire damage at initiative 2",
+				"989 units each with 1274 hit points (immune to fire; weak to bludgeoning, slashing) with an attack that does 25 slashing damage at initiative 3",
+				"",
+				"Infection:",
+				"801 units each with 4706 hit points (weak to radiation) with an attack that does 116 bludgeoning damage at initiative 1",
+				"4485 units each with 2961 hit points (immune to radiation; weak to fire, cold) with an attack that does 12 slashing damage at initiative 4",
+			},
+			Exp: 51},
+	}
+
+	for index, testcase := range data {
+		_, result := ImmuneSystemSimulator(testcase.Inp, 1570)
+		if result != testcase.Exp {
+			t.Errorf("%dth test was incorrect, got: %d, expected: %d.", index, result, testcase.Exp)
+		}
+	}
+}
