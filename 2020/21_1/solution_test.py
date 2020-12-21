08b3f0a2434dfc0fd5230ed46ec2dca835eaf61b
Mátyás Budavári <budavariam@gmail.com>
2020-12-21 21:43:48 +0100

    2020 day 21 / 1

---
+++ 2020/21_1/solution_test.py
@@ -0,0 +1,21 @@
+""" Advent of code 2020 day 21/1 """
+
+import unittest
+from solution import solution
+
+
+class MyTest(unittest.TestCase):
+    """Unist tests for actual day"""
+
+    def test_basic(self):
+        """ Test from the task """
+        self.assertEqual(solution("""\
+mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)\
+"""), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
