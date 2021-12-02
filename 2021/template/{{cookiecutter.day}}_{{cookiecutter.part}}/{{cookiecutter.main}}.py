7ab6a291ef143865c417aeae6e839fc0de738de1
Mátyás Budavári <budavariam@gmail.com>
2021-12-02 07:20:24 +0100

    2021 improve starter template

--- 2021/template/{{cookiecutter.day}}_{{cookiecutter.part}}/{{cookiecutter.main}}.py
+++ 2021/template/{{cookiecutter.day}}_{{cookiecutter.part}}/{{cookiecutter.main}}.py
@@ -2,6 +2,7 @@
 
 import math
 from os import path
+import re
 
 
 class Code(object):
@@ -9,11 +10,18 @@ class Code(object):
         self.lines = lines
 
     def solve(self):
-        pass
-
+        # print(self.lines)
+        for line in self.lines:
+            pass
 
 def preprocess(raw_data):
-    processed_data = raw_data.split("\n")
+    # pattern = re.compile(r'(\w+) (\d+)')
+    processed_data = []
+    for line in raw_data.split("\n"):
+        # match = re.match(pattern, line)
+        # data = [match.group(1), match.group(2)]
+        data = line
+        processed_data.append(data)
     return processed_data
 
 
