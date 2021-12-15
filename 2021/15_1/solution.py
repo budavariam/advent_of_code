1a2b2efc5c65e53cc70d85b1b0e600b526ebecd4
Mátyás Budavári <budavariam@gmail.com>
2021-12-15 09:13:33 +0100

    2021 day 15 / 1 Use heap

--- 2021/15_1/solution.py
+++ 2021/15_1/solution.py
@@ -3,13 +3,14 @@
 import math
 from os import path
 import re
+import heapq as hq
 
 import sys
 from collections import deque
 sys.setrecursionlimit(500000000)
 
 pos = [
-    (-1, 0), (1, 0), (0, -1), (0, 1), 
+    (-1, 0), (1, 0), (0, -1), (0, 1),
     # (-1, 1), (-1, -1), (1, -1), (1, 1)
 ]
 
@@ -19,7 +20,6 @@ class Code(object):
         self.lines = lines
         self.height = len(lines)
         self.width = len(lines[0])
-        self.globalmin = 0 #self.height * 4 * 9 + self.width * 4 * 9
 
     def fillpos(self, y, x):
         pl = []
@@ -37,55 +37,30 @@ class Code(object):
         # for line in self.lines:
         #     print("".join(map(str,['+' if x>9 else x for x in line])))
 
-    def paths(self, s, e, visited):
-        deq = deque([(s, visited, 0)])
-        res = deque([])
-        i = 0
+    def paths(self, s, e):
+        deq = [(0, s)]
+        res = []
 
-        for u in range(self.height):
-            for v in range(self.width):
-                if u == v:
-                    self.globalmin += self.lines[u][v]
-                    self.globalmin += self.lines[u][v-1]
-        print("heuristic",  self.globalmin)
-        self.globalmin = 774
-        while len(deq) > 0:
-            i+=1
-            if (i % 30000 == 0):
-                print("chk", c_pos, c_risk, self.globalmin, len(c_visited), len(deq))
-            [c_pos, c_visited, c_risk] = deq.pop()
-            if c_pos in c_visited or c_risk > self.globalmin:
-                continue
+        while deq:
+            [c_risk, c_pos] = hq.heappop(deq)
             if c_pos == e:
                 # get last item
-                print("finish with", c_risk, self.globalmin, len(c_visited), len(deq))
-                self.globalmin = min(c_risk, self.globalmin)
+                print("finish with", c_risk, len(deq))
                 res.append(c_risk)
             y, x = c_pos
-            next_pos = self.fillpos(y,x)
-            heu = []
+            next_pos = self.fillpos(y, x)
             for neighbour in next_pos:
-                if neighbour not in c_visited:
-                    n_vis = c_visited.copy().union(set([c_pos]))
-                    newrisk = c_risk + self.lines[neighbour[0]][neighbour[1]]
-                    if newrisk < self.globalmin:
-                        heu.append((neighbour, n_vis, newrisk))
-            heu = sorted(heu, key=lambda tup: tup[0][0] + tup[0][1])
-            if len(heu) > 0:
-                a = heu.pop()
-                deq.append(a) # add most end near to the top
-            heu2 = sorted(heu, key=lambda tup: tup[2], reverse=True)
-            if len(heu2) > 0:
-                deq.append(heu2[0]) # add smallest rank top to the start
-            if len(heu2) > 1:
-                for d in heu[1:]:
-                    deq.appendleft(d) # add lowest top to the end
-        return list(res)
+                n_y, n_x = neighbour
+                if self.lines[n_y][n_x] >= 0:
+                    newrisk = (c_risk + self.lines[n_y][n_x] % 9) + 1
+                    hq.heappush(deq, (newrisk, neighbour))
+                    self.lines[n_y][n_x] = -1
+        return res
 
     def solve(self):
         print(self.lines)
-        
-        risks = self.paths((0,0), (self.height-1, self.width-1), set())
+
+        risks = self.paths((0, 0), (self.height-1, self.width-1))
         print(risks)
         return min(risks)
 
@@ -97,7 +72,7 @@ def preprocess(raw_data):
         # match = re.match(pattern, line)
         # data = [match.group(1), match.group(2)]
         data = line
-        processed_data.append([int(x) for x in data])
+        processed_data.append([int(x)-1 for x in data])
     return processed_data
 
 
