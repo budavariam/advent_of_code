f237ab288f749a53a0567e47212cf4352d37645f
Mátyás Budavári <budavariam@gmail.com>
2022-12-05 06:43:26 +0100

    2022 day 05 / 2
    cleanup

--- 2022/05_2/solution.py
+++ 2022/05_2/solution.py
@@ -1,62 +1,61 @@
 """ Advent of code 2022 day 05 / 2 """
 
-import math
 from os import path
 import re
-from collections import defaultdict
 
 
-def is_empty(item):
-    if item == '':
-        return False
-    return True
-
-def get_result(crates):
-    return ''.join([x[-1] for x in crates])
-
-def move(inst, data):
-    crts = data
-    count, frm, to = inst
-    tmp = []
-    for _ in range(count):
-        popped = data[frm - 1].pop()
-        tmp.append(popped)
-    data[to - 1 ].extend(tmp[::-1])
-    return crts
+def not_empty(item):
+    return item != ""
 
 class Code(object):
     def __init__(self, input_data):
-        self.raw_crates = input_data[0]
-        self.crates = list(map(list, zip(*input_data[0])))
-        self.crates = [list(filter(is_empty, x[::-1])) for x in self.crates]
+        self.crates = input_data[0]
         self.instructions = input_data[1]
 
+    def get_result(self, crates):
+        return "".join([x[-1] for x in crates])
+
+    def move(self, inst, data):
+        crts = data
+        count, frm, to = inst
+        tmp = []
+        for _ in range(count):
+            popped = data[frm - 1].pop()
+            tmp.append(popped)
+        data[to - 1].extend(tmp[::-1])
+        return crts
+
     def solve(self):
-        print(self.crates)
+        # print(self.crates)
         for inst in self.instructions:
-            move(inst, self.crates)
-        return get_result(self.crates)
+            self.move(inst, self.crates)
+        return self.get_result(self.crates)
 
 
 def preprocess(raw_data):
-    pattern_instr = re.compile(r'move (\d+) from (\d+) to (\d+)')
+    pattern_instr = re.compile(r"move (\d+) from (\d+) to (\d+)")
     lines = raw_data.split("\n")
     i = 0
     crates = []
     instructions = []
     for line in lines:
         n = 4
-        crts=[line[i:i+n].strip().strip('[]') for i in range(0, len(line), n)]
-        i+=1
-        if line == '':
+        crts = [line[i : i + n].strip().strip("[]") for i in range(0, len(line), n)]
+        i += 1
+        if line == "":
             break
-        if crts[0] == '1':
+        if crts[0] == "1":
             continue
         crates.append(crts)
+
+    crates = list(map(list, zip(*crates)))
+    crates = [list(filter(not_empty, x[::-1])) for x in crates]
+
     for line in lines[i:]:
         match = re.match(pattern_instr, line)
         data = [int(x) for x in [match.group(1), match.group(2), match.group(3)]]
         instructions.append(data)
+
     return (crates, instructions)
 
 
