df22ec21ee13dd03e2f1998cdf2d9a791d93c925
Mátyás Budavári <budavariam@gmail.com>
2022-12-11 09:00:29 +0100

    2022 day 11 / 1

---
+++ 2022/11_1/solution.py
@@ -0,0 +1,84 @@
+""" Advent of code 2022 day 11 / 1 """
+
+from os import path
+import re
+
+
+class Monkey(object):
+    def __init__(self, m_id, items, operation, test, if_true, if_false):
+        self.m_id = m_id
+        self.items = items
+        self.operation = operation
+        self.test = test
+        self.if_true = if_true
+        self.if_false = if_false
+        self.activity_cnt = 0
+
+    def __repr__(self):
+        return f"{self.m_id}: {self.activity_cnt} (#{len(self.items)})"
+
+    def round(self):
+        result = []
+        # pylint: disable=unused-variable
+        for item_value in self.items:
+            self.activity_cnt += 1
+
+            math_op = self.operation[0]
+            other_val = self.operation[1].replace("old", "item_value")
+            # pylint: disable=eval-used
+            new_level = eval(f"item_value {math_op} {other_val}") // 3
+
+            who = self.if_true if new_level % self.test == 0 else self.if_false
+            what = new_level
+            result.append((who, what))
+        # throw all away
+        self.items = []
+        return result
+
+
+class Code(object):
+    def __init__(self, lines):
+        self.monkeys = lines
+
+    def solve(self):
+        print(self.monkeys)
+        for _ in range(20):
+            for m_id in range(len(self.monkeys)):
+                thrown_items = self.monkeys[m_id].round()
+                for (which_m, what) in thrown_items:
+                    self.monkeys[which_m].items.append(what)
+        self.monkeys.sort(key=lambda x: x.activity_cnt, reverse=True)
+
+        return self.monkeys[0].activity_cnt * self.monkeys[1].activity_cnt
+
+
+def preprocess(raw_data):
+    processed_data = []
+    for m_data in raw_data.split("\n\n"):
+        data = m_data.split("\n")
+        m_id = int(re.match(r"Monkey (\d+):", data[0]).group(1))
+        start_items = [int(x) for x in data[1].split(": ")[1].split(", ")]
+
+        ops = data[2].split("new = old ")[1].split(" ")
+        operation = (ops[0], ops[1])
+
+        test = int(data[3].split("divisible by ")[1])
+
+        if_true = int(data[4].split("monkey ")[1])
+        if_false = int(data[5].split("monkey ")[1])
+
+        m = Monkey(m_id, start_items, operation, test, if_true, if_false)
+        processed_data.append(m)
+    return processed_data
+
+
+def solution(data):
+    """Solution to the problem"""
+    lines = preprocess(data)
+    solver = Code(lines)
+    return solver.solve()
+
+
+if __name__ == "__main__":
+    with (open(path.join(path.dirname(__file__), "input.txt"), "r")) as input_file:
+        print(solution(input_file.read()))
---
+++ 2022/11_1/solution_test.py
@@ -0,0 +1,42 @@
+""" Advent of code 2022 day 11 / 1 """
+
+import unittest
+from solution import solution
+
+
+class MyTest(unittest.TestCase):
+    """Unist tests for actual day"""
+
+    def test_basic(self):
+        """Test from the task"""
+        self.assertEqual(solution("""Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1"""), 10605)
+
+
+if __name__ == "__main__":
+    unittest.main()
