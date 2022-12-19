1310b6a2c1b913f58fc6889facd7279132f9d054
Mátyás Budavári <budavariam@gmail.com>
2022-12-19 22:17:21 +0100

    2022 day 19 / 2

---
+++ 2022/19_2/solution_test.py
@@ -0,0 +1,17 @@
+""" Advent of code 2022 day 19 / 2 """
+
+import unittest
+from solution import solution
+
+
+class MyTest(unittest.TestCase):
+    """Unist tests for actual day"""
+
+    def test_basic(self):
+        """Test from the task"""
+        self.assertEqual(solution("""Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian."""), 3348)
+
+
+if __name__ == "__main__":
+    unittest.main()
