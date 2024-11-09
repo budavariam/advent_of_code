9c9c019db55cc2321dec437443d44f6d82437186
Mátyás Budavári <budavariam@gmail.com>
2024-11-09 20:53:23 +0100

    2023 day 17 / 1

---
+++ 2023/17_1/solution_test.py
@@ -0,0 +1,33 @@
+""" Advent of code 2023 day 17 / 1 """
+
+import unittest
+from solution import solution
+
+
+class MyTest(unittest.TestCase):
+    """Unist tests for actual day"""
+
+    def test_basic(self):
+        """Test from the task"""
+        self.assertEqual(
+            solution(
+                """2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533"""
+            ),
+            102,
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
