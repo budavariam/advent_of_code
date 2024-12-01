09c36b7d7ddcb16602a615ee33768827f5c0c3d5
Mátyás Budavári <budavariam@gmail.com>
2024-12-01 07:47:13 +0100

    2024 day 01 / 1

---
+++ 2024/01_1/solution.py
@@ -0,0 +1,51 @@
+""" Advent of code 2023 day 01 / 1 """
+
+import math
+from pprint import pprint
+from os import path
+import re
+from collections import defaultdict
+from utils import log, profiler
+
+
+class Code(object):
+    def __init__(self, lines):
+        self.lines = lines
+
+    def solve(self):
+        pprint(self.lines)
+        result = 0
+        a = sorted([x[0] for x in self.lines])
+        b = sorted([x[1] for x in self.lines])
+        for i, _ in enumerate(self.lines):
+            result += abs(b[i] - a[i])
+        return result
+
+
+@profiler
+def preprocess(raw_data):
+    pattern = re.compile(r"(\d+)\s+(\d+)")
+    processed_data = []
+    for line in raw_data.split("\n"):
+        match = re.match(pattern, line)
+        data = [int(match.group(1)), int(match.group(2))]
+        # data = line
+        processed_data.append(data)
+    return processed_data
+
+
+@profiler
+def solution(data):
+    """Solution to the problem"""
+    lines = preprocess(data)
+    solver = Code(lines)
+    return solver.solve()
+
+
+if __name__ == "__main__":
+    with open(
+        path.join(path.dirname(__file__), "input.txt"),
+        "r",
+        encoding="utf-8",
+    ) as input_file:
+        print(solution(input_file.read()))
---
+++ 2024/01_1/solution_test.py
@@ -0,0 +1,21 @@
+""" Advent of code 2023 day 01 / 1 """
+
+import unittest
+from solution import solution
+
+
+class MyTest(unittest.TestCase):
+    """Unist tests for actual day"""
+
+    def test_basic(self):
+        """Test from the task"""
+        self.assertEqual(solution("""3   4
+4   3
+2   5
+1   3
+3   9
+3   3"""), 11)
+
+
+if __name__ == "__main__":
+    unittest.main()
