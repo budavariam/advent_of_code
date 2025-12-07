098acc17424a6ec919fad0d5946abd161c084631
Mátyás Budavári <budavariam@gmail.com>
2025-12-07 20:48:39 +0100

    2025 day 01 / 1

---
+++ 2025/01_1/solution_test.py
@@ -0,0 +1,26 @@
+""" Advent of code 2025 day 01 / 1 """
+
+import unittest
+from solution import solution
+
+
+class MyTest(unittest.TestCase):
+    """Unist tests for actual day"""
+
+    def test_basic(self):
+        """Test from the task"""
+        self.assertEqual(solution("""L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+"""), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
