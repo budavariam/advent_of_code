a1950fb5ad2856ba1407c56d0f7336b1e6990bd8
Mátyás Budavári <budavariam@gmail.com>
2025-12-07 21:09:43 +0100

    2025 day 02 / 2

---
+++ 2025/02_2/solution.py
@@ -0,0 +1,53 @@
+""" Advent of code 2024 day 02 / 2 """
+
+import math
+from pprint import pprint
+from os import path
+import re
+from collections import defaultdict
+from utils import log, profiler
+
+def is_invalid(s):
+    i = (s+s).find(s, 1, -1)
+    # return 1None if i == -1 else s[:i]
+    return i != -1
+
+class Code(object):
+    def __init__(self, lines):
+        self.lines = lines
+
+    def solve(self):
+        pprint(self.lines)
+        result = 0
+        for s,e in self.lines:
+            for current_id in range(s, e+1):
+                if is_invalid(str(current_id)):
+                    result += current_id
+                    print(f"Invalid ID found: {current_id}")
+        return result
+
+
+@profiler
+def preprocess(raw_data):
+    processed_data = []
+    for line in raw_data.split(","):
+        data = tuple(map(int, line.split("-")))
+        processed_data.append(data)
+    return processed_data
+
+
+@profiler
+def solution(data):
+    """Solution to the problem"""
+    lines = preprocess(data)
+    solver = Code(lines)
+    return solver.solve()
+
+
+if __name__ == "__main__":
+    with open(
+        path.join(path.dirname(__file__), "input.txt"),
+        "r",
+        encoding="utf-8",
+    ) as input_file:
+        print(solution(input_file.read()))
---
+++ 2025/02_2/solution_test.py
@@ -0,0 +1,16 @@
+""" Advent of code 2024 day 02 / 2 """
+
+import unittest
+from solution import solution
+
+
+class MyTest(unittest.TestCase):
+    """Unist tests for actual day"""
+
+    def test_basic(self):
+        """Test from the task"""
+        self.assertEqual(solution("""11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""), 4174379265)
+
+
+if __name__ == "__main__":
+    unittest.main()
