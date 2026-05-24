01195f6346f522a4112c7f4b517f8eb205036e64
Mátyás Budavári <budavariam@gmail.com>
2026-05-24 23:11:36 +0200

    2015 day 20 / 1

---
+++ 2015/20_1/src/main.rs
@@ -0,0 +1,39 @@
+use std::{collections::HashSet, fs};
+
+fn find_divisors(num: i32) -> HashSet<i32> {
+    let sqrt = (num as f64).sqrt().floor() as i32;
+    let mut res: HashSet<i32> = HashSet::new();
+    for i in 1..=sqrt {
+        if num % i == 0 {
+            res.insert(i);
+            res.insert(num / i);
+        }
+    }
+    res
+}
+
+fn solution(input: &str) -> String {
+    let target: i32 = input.parse().unwrap();
+    let mut curr = 0;
+    let mut curr_max = 0;
+    while curr_max < target {
+        curr += 1;
+        let divisors = find_divisors(curr);
+        let present_cnt = divisors.iter().map(|e| e * 10).sum();
+        curr_max = curr_max.max(present_cnt);
+        if curr % 100_000 == 0 || curr_max >= target {
+            println!("{curr}: {divisors:?} = {present_cnt}");
+        }
+    }
+
+    curr.to_string()
+}
+
+fn main() {
+    let input = fs::read_to_string("input.txt").expect("Failed to read input.txt");
+    let answer = solution(&input);
+    println!("Answer: {}", answer);
+}
+
+#[cfg(test)]
+mod main_tests;
